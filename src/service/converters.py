10f3ebe578451670e13684d26b195f560f567ec5
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2022-05-21 18:26:32 +0400

    Added flask dependencies and boilerplate code

--- src/service/converters.py
+++ src/service/converters.py
@@ -14,10 +14,12 @@ def jpg2pdf(jpg_path: str, pdf_path: str) -> None:
         _convert_and_save_pdf(image, pdf)
 
 def _convert_and_save_pdf(image, pdf) -> None:
-    pdf_bytes = img2pdf.convert(image.filename)
+    pdf_bytes = _convert_to_pdf(image)
     with _create_pdf(pdf.dest_path) as file:
         file.write(pdf_bytes)
 
+def _convert_to_pdf(image) -> bytes:
+    return img2pdf.convert(image.filename)
 
 def _get_jpg_pdf(jpg_path, pdf_path) -> tuple[model.JPG, model.PDF]:
     jpg = model.allocate_jpeg(src_path=jpg_path)
