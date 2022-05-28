44a1424768bee1a6ce2ffa58b7621c8c109b2390
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2022-05-28 18:44:36 +0400

    added first endpoint and mocked use cases for the flask run

--- src/serializers/model.py
+++ src/serializers/model.py
@@ -15,3 +15,15 @@ class JPGJsonEncoder(json.JSONEncoder):
             return {"code": str(o.code), "src_path": o.src_path, "extensions": f"{list(o.extensions)}"}
         except AttributeError:
             return super().default(o) 
+        
+
+class ConvertedJsonEncoder(json.JSONEncoder):
+    def default(self, o: Any) -> Any:
+        try:
+            return {
+                "converted_from": json.dumps(o.converted_from, cls=JPGJsonEncoder),
+                "converted_to": json.dumps(o.converted_to, cls=PDFJsonEncoder),
+                "converted_at": str(o.converted_at)
+            }
+        except AttributeError:
+            return super().default(o)
