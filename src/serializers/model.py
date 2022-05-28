36179b404452f8ef17d651c9cad6d48ddfacde27
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2022-05-28 17:43:29 +0400

    added json model serializers to pdf and jpg, added tests

---
+++ src/serializers/model.py
@@ -0,0 +1,17 @@
+import json
+from typing import Any
+
+
+class PDFJsonEncoder(json.JSONEncoder):
+    def default(self, o: Any) -> Any:
+        try:
+            return {"code": str(o.code), "dest_path": o.dest_path, "extension": o.extension}
+        except AttributeError:
+            return super().default(o)
+        
+class JPGJsonEncoder(json.JSONEncoder):
+    def default(self, o: Any) -> Any:
+        try:
+            return {"code": str(o.code), "src_path": o.src_path, "extensions": f"{list(o.extensions)}"}
+        except AttributeError:
+            return super().default(o) 
